df3a6362bf61a91ced3fe2725dc31d338e71cf78
MKREEP <mkreep4ik@gmail.com>
2022-01-13 01:04:14 +0300

    prototype_v2

--- MAIN/transp_db.py
+++ MAIN/transp_db.py
@@ -8,7 +8,12 @@ from sqlite3.dbapi2 import Cursor
 
 
 application = Flask(__name__)
-@application.route('/', methods=['POST', 'GET'])
+
+@application.route('/')
+def mainpage():
+	return render_template('mainpage.html')
+
+@application.route('/login', methods=['POST', 'GET'])
 def index():
 	username = ''
 	password = ''
@@ -23,20 +28,15 @@ def index():
 		result_login = str(cur.execute('SELECT login FROM logins_passwords WHERE login=?;', (username,)).fetchone()[0])
 		result_password = str(cur.execute('SELECT password FROM logins_passwords WHERE login=?;', (username,)).fetchone()[0])
 		if password == result_password:
-			return redirect('mainpage')
+			return redirect('home')
 		else:
 			return redirect('error_login')
 
 	return render_template('index.html')
 
-	# else:
-	# 	message = 'Введите ваш логин и пароль.'
-	# return render_template('index.html', message=message)
-
-@application.route('/mainpage')
-def mainpage():
-	#member_NickName = 
-	return render_template('main.html')
+@application.route('/home')
+def personal_page():
+	return render_template('personal.html')
 
 @application.errorhandler(500)
 def error_login(e):
@@ -63,17 +63,21 @@ def reg():
 		member_Login = str(request.form.get('member_Login'))
 		member_Password = str(request.form.get('member_Password'))
 
-	conn = sqlite3.connect('ProjectMembers.db')
-	cur = conn.cursor()
-	print('БД подключена к SQLite успешно.')
+		conn = sqlite3.connect('ProjectMembers.db')
+		cur = conn.cursor()
+		print('БД подключена к SQLite успешно.')
 
-	member_RegData = (member_NickName, member_DiscordTag, member_EMail, member_Discord)
-	cur.execute("INSERT INTO members(NickName, DiscordTag, Email, Discord) VALUES(?, ?, ?, ?);", member_RegData)
-	conn.commit()
+		loginCheck = str(cur.execute('SELECT login FROM logins_passwords WHERE login=?;', (member_Login,)).fetchone()[0])
+		if loginCheck == member_Login:
+			pass
 
-	login_RegData = (member_Login, member_Password)
-	cur.execute("INSERT INTO logins_passwords(login, password) VALUES(?, ?);", login_RegData)
-	conn.commit()
+		member_RegData = (member_NickName, member_DiscordTag, member_EMail, member_Discord)
+		cur.execute("INSERT INTO members(NickName, DiscordTag, Email, Discord) VALUES(?, ?, ?, ?);", member_RegData)
+		conn.commit()
+
+		login_RegData = (member_Login, member_Password)
+		cur.execute("INSERT INTO logins_passwords(login, password) VALUES(?, ?);", login_RegData)
+		conn.commit()
 
 	return render_template('reg_page.html')
 
